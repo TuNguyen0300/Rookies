97e122397d55caf383a374b2abcedf8367893143
TuNguyen0300 <nguyenhuutu21032000@gmail.com>
2022-11-21 18:48:18 +0700

    commit final

---
+++ SeleniumAdvantage_Final/testFramework/Testcases/TestScenario2.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using testFramework.PageObject;
+using testFramework.TestSetup;
+
+namespace testFramework.Testcases
+{
+    [TestFixture]
+    public class TestScenario2 : ProjectNUnitTestSetup
+    {
+        [Test]
+        public void TestAuto() 
+        {
+            AddPage AddPage = new AddPage(_driver);
+            AddPage.SelectElement();
+            AddPage.CheckDisplayElement();
+            AddPage.SelectWebTable();
+            AddPage.CheckDisplayWebTable();
+            AddPage.GoToAddScreen();
+            AddPage.EnterValidData();
+            AddPage.ClickSubmit();
+        }        
+
+        /*[Test]
+        public void Id1_VerifySearchResultPageTitle()
+        {
+            UserCanSearchKeyword();
+            SearchResultPage searchResultPage = new SearchResultPage(_driver);
+            searchResultPage.ComparePageTitle("test text - Google Search");
+        }*/
+
+    }
+}
---
+++ SeleniumAdvantage_Final/testFramework/Testcases/TestScenario3.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using testFramework.PageObject;
+using testFramework.TestSetup;
+
+namespace testFramework.Testcases
+{
+    [TestFixture]
+    public class TestScenario3 : ProjectNUnitTestSetup
+    {
+        [Test]
+        public void TestAuto() 
+        {
+            EditPage editPage = new EditPage(_driver);
+            editPage.SelectElement();
+            editPage.CheckDisplayElement();
+            editPage.SelectWebTable();
+            editPage.CheckDisplayWebTable();
+            editPage.GoToEditScreen();
+
+        }
+
+    }
+}
